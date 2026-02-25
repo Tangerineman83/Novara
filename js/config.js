2d7c54a177458215fff3c887d132cd2df08bde29
Tangerineman83 <callumstewart86@gmail.com>
2026-02-25 07:01:23 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -85,31 +85,4 @@ export const PRESET_STRATEGIES = [
         name: "Default Balanced",
         points: [
             { years: 50, weights: { globalEq: 0.79, realEstate: 0.02, listedAlts: 0.05, emDebt: 0.05, igCredit: 0.09 } },
-            { years: 15, weights: { globalEq: 0.79, realEstate: 0.02, listedAlts: 0.05, emDebt: 0.05, igCredit: 0.09 } },
-            { years: 0,  weights: { globalEq: 0.35, realEstate: 0.02, listedAlts: 0.08, emDebt: 0.1, igCredit: 0.2, sdCredit: 0.25 } }
-        ]
-    },
-    {
-        name: "100% Growth",
-        points: [
-            { years: 50, weights: { globalEq: 0.93, realEstate: 0.02, listedAlts: 0.05 } },
-            { years: 15, weights: { globalEq: 0.93, realEstate: 0.02, listedAlts: 0.05 } },
-            { years: 0,  weights: { globalEq: 0.35, realEstate: 0.02, listedAlts: 0.08, emDebt: 0.1, igCredit: 0.2, sdCredit: 0.25 } }
-        ]
-    },
-    {
-        name: "Diversified Private Assets",
-        points: [
-            { years: 50, weights: { globalEq: 0.6975, privAssets: 0.25, realEstate: 0.015, listedAlts: 0.0375 } },
-            { years: 15, weights: { globalEq: 0.6975, privAssets: 0.25, realEstate: 0.015, listedAlts: 0.0375 } },
-            { years: 0,  weights: { globalEq: 0.35, realEstate: 0.02, listedAlts: 0.08, emDebt: 0.1, igCredit: 0.2, sdCredit: 0.25 } }
-        ]
-    },
-    {
-        name: "Simple Glidepath",
-        points: [
-            { years: 40, weights: { globalEq: 0.8, igCredit: 0.2 } },
-            { years: 0,  weights: { globalEq: 0.2, igCredit: 0.8 } }
-        ]
-    }
-];
+            { years: 15, weights:
